1c336291f269c6713b8bf2f395c8bb571ebb20d2
alwin <alwin24012002@gmail.com>
2022-07-23 11:22:42 +0530

    Able to collect platformFee

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,9 @@ import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import "../styles/App.css";
 import { JupiterProvider } from "@jup-ag/react-hook";
+import { getPlatformFeeAccounts } from "@jup-ag/core";
+import { PublicKey } from "@solana/web3.js";
+import { useEffect, useState } from "react";
 
 export const SECOND_TO_REFRESH = 30 * 1000;
 
@@ -28,14 +31,31 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 const JupiterWrapper: React.FC = ({ children }) => {
+  const [platformFeesAccount, setPlatformFeesAccount] = useState<{ feeBps: number, feeAccounts: Map<string, PublicKey> }>();
   const { connection } = useConnection();
   const wallet = useWallet();
+
+  useEffect(() => {
+    const setFeeAccount = async () => {
+      let platformFeeAndAccounts = {
+        feeBps: 255,
+        feeAccounts: await getPlatformFeeAccounts(
+          connection, new PublicKey("5mfkNCR8Fo5p3DLHYBW6knuArGEgj8Ui2Lm4qnWEWUFh") // The platform fee account owner
+        ) // map of mint to token account pubkey
+      };
+      console.log(platformFeeAndAccounts.feeAccounts);
+
+      setPlatformFeesAccount(platformFeeAndAccounts);
+    };
+    setFeeAccount();
+  }, [])
   return (
     <JupiterProvider
       cluster="mainnet-beta"
       connection={connection}
       userPublicKey={wallet.publicKey || undefined}
       routeCacheDuration={SECOND_TO_REFRESH}
+      platformFeeAndAccounts={platformFeesAccount}
     >
       {children}
     </JupiterProvider>
